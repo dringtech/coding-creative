4f53926168d232fa07728dbc24263f744d42d205
Giles Dring <gilesdring@gmail.com>
2019-08-29 23:00:36 +0100

    Adding documentation and generated examples

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-mkdir -p output
-git archive --format zip --output output/coding-creative.zip master examples*
+example_name=coding-creative-examples
+mkdir -p docs/examples
+git archive --format zip --output docs/examples/${example_name}.zip master examples*
+cp examples.properties docs/examples/${example_name}.properties
