a4881fd4482d78a5e6c50f4faf8cab7618537478
Giles Dring <gilesdring@gmail.com>
2019-09-02 16:05:02 +0100

    Prefixing archive file with examples name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 example_name=coding-creative-examples
 mkdir -p docs/examples
-git archive --format zip --output docs/examples/${example_name}.zip master examples*
+git archive --format zip --output docs/examples/${example_name}.zip --prefix ${example_name}/ master examples*
 cp examples.properties docs/examples/${example_name}.txt
